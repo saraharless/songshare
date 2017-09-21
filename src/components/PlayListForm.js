2dfd7d5ab22efcb8e73f379a4c079fe844c90f23
saraharless <sara.e.harless@gmail.com>
2017-09-20 21:34:57 -0500

    basic form styling

--- src/components/PlayListForm.js
+++ src/components/PlayListForm.js
@@ -17,18 +17,18 @@ export default class NavBar extends Component {
       <div>
 
       <form onSubmit={this.addToList}>
-<input name="username" type="text" placeholder="Enter your UserName"
+<input name="username" type="text" placeholder="UserName"
 onChange={this.handleUserChange} value={this.state.userName}/>
 
-<input name="artist" type="text" placeholder="Artist Name"
+<input name="artist" type="text" placeholder="Artist or Band Name"
 onChange={this.handleArtistChange} value={this.state.songArtist}/>
 
 <input name="title" type="text" placeholder="Song Title"
 onChange={this.handleTitleChange} value={this.state.songTitle}/>
 
-<textarea rows="4" cols="50" name="notes" type="text" placeholder="Notes"  onChange={this.handleNotesChange} value={this.state.songNotes}></textarea>
+<textarea rows="4" cols="50" name="notes" type="text" placeholder="Notes About Song"  onChange={this.handleNotesChange} value={this.state.songNotes}></textarea>
 
-<button type="submit">Submit</button>
+<button type="submit" className="btn">Submit</button>
 
         </form>
 
