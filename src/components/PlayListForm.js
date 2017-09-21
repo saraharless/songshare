d6bb6ef4c34ef079f58aa31e3c12794846e96779
saraharless <sara.e.harless@gmail.com>
2017-09-20 21:29:04 -0500

    add basic form, constructor, props and state

--- src/components/PlayListForm.js
+++ src/components/PlayListForm.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 export default class NavBar extends Component {
+    constructor(props){
+      super(props);
+
+    this.state = {
+      userName: "",
+      songArtist: "",
+      songTitle: "",
+      SongNotes: ""
+    };
+    }
+
   render() {
     return (
       <div>
-        <h1>Form</h1>
+
+      <form onSubmit={this.addToList}>
+<input name="username" type="text" placeholder="Enter your UserName"
+onChange={this.handleUserChange} value={this.state.userName}/>
+
+<input name="artist" type="text" placeholder="Artist Name"
+onChange={this.handleArtistChange} value={this.state.songArtist}/>
+
+<input name="title" type="text" placeholder="Song Title"
+onChange={this.handleTitleChange} value={this.state.songTitle}/>
+
+<textarea rows="4" cols="50" name="notes" type="text" placeholder="Notes"  onChange={this.handleNotesChange} value={this.state.songNotes}></textarea>
+
+<button type="submit">Submit</button>
+
+        </form>
+
       </div>
     )
   }
