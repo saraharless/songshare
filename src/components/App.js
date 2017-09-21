62a6da3f0683e5ed60c2ad9921fe4d98460fa3af
saraharless <sara.e.harless@gmail.com>
2017-09-20 21:24:34 -0500

    setup file structure for page, add imports and exports, page renders without errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
+import PlayList from './PlayList.js';
+import PlayListForm from './PlayListForm.js';
+import NavBar from './NavBar.js';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <NavBar/>
+      <div>
+        <h1>Song Share</h1>
       </div>
+      <PlayListForm/>
+      <PlayList/>
+      </div>
+
     );
   }
 }
-
-export default App;
---
+++ src/components/PlayListForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+      </div>
+    )
+  }
+}
